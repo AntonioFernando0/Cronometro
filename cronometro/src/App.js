5f021bb27e854c145d16b3b05abc33688ec60a2a
Antonio Fernando <jufer376@gmail.com>
2025-03-12 20:39:47 -0300

    Projeto do cronometro

--- cronometro/src/App.js
+++ cronometro/src/App.js
@@ -6,22 +6,61 @@ import './style.css'
 class App extends Component {
     constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      numero: 0,
+      botao: 'INICIAR'
     }
+    this.iniciar = this.iniciar.bind(this);
+    this.limpar = this.limpar.bind(this);
+    this.timer = null;
+
+
+    }
+
+    iniciar(){
+      let state = this.state
+
+      if(this.timer !== null){
+        clearInterval(this.timer);
+        this.timer = null;
+        state.botao = 'INICIAR'
+      }else{  
+        this.timer = setInterval(()=> {
+        let state = this.state;
+        state.numero += 0.1;
+        this.setState(state);
+    }, 100);
+    state.botao = 'PAUSAR'
+
+  }
+   this.setState(state);
+        
+    }
+
+    limpar(){
+      
+        this.timer = setInterval(() => {
+          let state = this.state;
+          state.numero = 0.0
+          this.setState(state)
+        })
+
+    }
+
     render(){
       return(
 
         <div className='container'>
 
             <img src={require('./assets/images.png')}/>
-          <a className='Timer'>0.0</a>
+          <a className='Timer'>{this.state.numero.toFixed(1)}</a>
 
           <div className='AreaBtn'>
-            <a className='botao'>
-              INICIAR
+            <a className='botao' onClick={this.iniciar}>
+              {this.state.botao}
             </a>
 
-            <a className='botao'>
+            <a className='botao' onClick={this.limpar}>
               LIMPAR
             </a>
           </div>
